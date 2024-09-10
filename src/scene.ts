455cfdafc768945f811bc87f67c166e09892ad2e
Marecar Rayan <rayanminh.marecar@gmail.com>
2024-09-10 11:29:34 +0200

    adding 3D model and event

--- src/scene.ts
+++ src/scene.ts
@@ -25,20 +25,52 @@ export function createScene(renderer: WebGLRenderer) {
     20,
   );
 
-  const boxGeometry = new BoxBufferGeometry(1, 1, 1);
-  const boxMaterial = new MeshBasicMaterial({color: 0xff0000});
-  const box = new Mesh(boxGeometry, boxMaterial);
-  box.position.z = -3;
+  // adding the 3D Model
 
-  scene.add(box);
-  
-  const renderLoop = (timestamp: number, frame ?:XRFrame) => {
-    box.rotation.y += 0.03;
-    box.rotation.x += 0.03;
+  let koalaModel: Object3D;
 
-    if (renderer.xr.isPresenting)
-        renderer.render(scene, camera);
+  const gltfLoader = new GLTFLoader();
+
+  gltfLoader.load("../assets/models/koala.glb", (gltf: GLTF) => {koalaModel = gltf.scene.children[0];});
+  const planeMarker = createPlaneMarker();
+
+  // adding ambient light
+  const ambientLight = new AmbientLight(0xffffff, 1.0);
+  scene.add(ambientLight);
+  // adding WebXR controller
+
+  const controller = renderer.xr.getController(0);
+  scene.add(controller);
+  // adding the event
+  controller.addEventListener("select", onSelect);
+
+  function onSelect() {
+    if (planeMarker.visible) {
+        const model = koalaModel.clone();
+        model.position.setFromMatrixPosition(planeMarker.matrix);
+
+        model.rotation.y = Math.random() * (Math.PI * 2);
+        model.visible = true;
+        scene.add(model)
+    }
   }
+  scene.add(planeMarker);
 
+
+  const renderLoop = (timestamp: number, frame ?:XRFrame) => {
+
+    if (renderer.xr.isPresenting) {
+        if (frame) {
+            handleXRHitTest(renderer, frame, (HitPoseTransformed: Float32Array) => {
+                if (HitPoseTransformed) {
+                    planeMarker.visible = true;
+                    planeMarker.matrix.fromArray(HitPoseTransformed);
+                }
+            }, () => {planeMarker.visible = false;})
+
+        }
+        renderer.render(scene, camera);
+    }
+  }
   renderer.setAnimationLoop(renderLoop);
 };
